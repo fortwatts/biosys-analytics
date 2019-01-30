33292d96cb1e820bfcfcb49bc0ebb2fbfb6ce3d1
unknown <gwatts@email.arizona.edu>
2019-01-30 10:47:51 -0700

    working

--- assignments/02-bash-scripting/cat-n.sh
+++ assignments/02-bash-scripting/cat-n.sh
@@ -3,12 +3,12 @@
 set -u
 
 if [[ $# -lt 1 ]] || [[ $# -gt 2 ]]; then
-    printf "Usage: %s files/sonnet-29.txt \n" "$(basename "$0")"
+    printf "Usage: %s FILE \n" "$(basename "$0")"
     exit 1
 fi
 
 if [[ ! -f $1 ]]; then
-    printf "Files/sonnet-29.txt is not a file\n"
+        printf "%s is not a file\n" "$1"
     exit 1
 fi
 
@@ -16,10 +16,9 @@ FILE=$1
 
 i=0
 while read -r LINE; do
-    if [[ i -lt 10 ]]; then
-	let i++
+    #if [[ i -lt 10 ]]; then
+        i=$((i+1))
         printf "%3d: %s\n" $i "$LINE"
-    else
-        exit 1
-    fi
+    #fi
 done < "$FILE"
+
---
+++ assignments/02-bash-scripting/head.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -u
+
+if [[ $# -lt 1 ]] || [[ $# -gt 2 ]]; then
+    printf "Usage: %s FILE [NUMLINES]\n" "$(basename "$0")"
+    exit 1
+fi
+
+FILE=$1
+NUM_ITERATIONS=${2:-3}
+
+i=0
+while read -r LINE; do
+    if [[ i -lt $NUM_ITERATIONS ]]; then
+    i=$((i+1))
+    printf "%s\n" "$LINE"
+    fi
+done < "$FILE"
+
