e19506d605a9057b57fafeaba136d45bf26d7390
unknown <gwatts@email.arizona.edu>
2019-01-30 10:57:39 -0700

    fixed file checking

--- assignments/02-bash-scripting/head.sh
+++ assignments/02-bash-scripting/head.sh
@@ -10,6 +10,11 @@ fi
 FILE=$1
 NUM_ITERATIONS=${2:-3}
 
+if [[ ! -f "$FILE" ]]; then
+	printf "$FILE is not a file.\n"
+	exit 1
+fi
+
 i=0
 while read -r LINE; do
     if [[ i -lt $NUM_ITERATIONS ]]; then
