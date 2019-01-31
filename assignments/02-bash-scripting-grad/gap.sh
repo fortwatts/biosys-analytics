40112e5ed5a12664c973e854a46a28407847e69f
gwatts <gwatts@email.arizona.edu>
2019-01-30 17:15:52 -0700

    gap.sh homework assignment

---
+++ assignments/02-bash-scripting-grad/gap.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -u
+
+if [[ $# -eq 0 ]]; then
+    REGEX='[A-Z]'
+#    echo "REGEX is set to: $REGEX because args = 0"
+elif [[ $1 =~ ^['['] ]]
+then
+    REGEX=$1
+else
+    REGEX="[$1]"
+fi
+
+UPPERCASEREGEX=${REGEX^^}
+#echo "UPPERCASEREGEX is set to : $UPPERCASEREGEX"
+
+i=0
+for FILENAME in $(ls -f1 ../../data/gapminder | sort); do
+    if [[ $FILENAME =~ ^$UPPERCASEREGEX ]]; then
+        BASENAME=$(basename $FILENAME '.cc.txt')
+	i=$((i+1))
+        echo "$BASENAME"
+    fi
+done
+
+if [[ $i -eq 0 ]]; then
+    echo "There are no countries starting with \"$REGEX\""
+fi
---
+++ assignments/02-bash-scripting-grad/hello.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -u
+
+if [[ $# -eq 0 ]] || [[ $# -gt 2 ]]; then
+    printf "Usage: %s GREETING [NAME] \n" "$(basename "$0")"
+    exit 1
+fi
+
+GREETING=$1
+NAME=${2:-Human}
+
+echo "$GREETING, $NAME!"
