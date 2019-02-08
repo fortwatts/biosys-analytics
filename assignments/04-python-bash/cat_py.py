51d702cffb18e5ca578d7f5c77102abceab57eb3
unknown <gwatts@email.arizona.edu>
2019-02-07 21:31:42 -0700

    cat.py to test on ocelote

--- assignments/04-python-bash/cat_py.py
+++ assignments/04-python-bash/cat_py.py
@@ -11,20 +11,27 @@ import sys
 
 # --------------------------------------------------
 def main():
-    file = sys.argv[1:]
+    args = sys.argv[1:]
+#    print('file is:', file)
 
     if len(args) != 1:
         print('Usage: {} FILE'.format(os.path.basename(sys.argv[0])))
         sys.exit(1)
 
-#### get the file lines into a list then:
-#### use enumereate on the list:
-for i, listelement in enumerate(lines in open(file).readines()):
-    print (i+1, listelement)
-#we increment 1 so we don't start at 0
+    file = args[0]
 
-#    print('Arg is "{}"'.format(arg))
+# get the file lines into a list then:
+    lines = []
+    for line in open(file):
+        lines.append(line.rstrip('\n'))
 
+# use enumereate on the list:
+
+    for i, listelement in enumerate(lines):
+        print(i+1, listelement)
+# we increment 1 so we don't start at 0
+
+# print('Arg is "{}"'.format(arg))
 
 # --------------------------------------------------
 main()
