8043fefe065353f461bee2fdc03b6aba8bb7192e
fortwatts <gwatts@email.arizona.edu>
2019-02-10 19:20:55 -0700

    cat-n.py for testing

--- assignments/04-python-bash/cat_py.py
+++ assignments/04-python-bash/cat_py.py
@@ -12,26 +12,23 @@ import sys
 # --------------------------------------------------
 def main():
     args = sys.argv[1:]
-#    print('file is:', file)
 
     if len(args) != 1:
         print('Usage: {} FILE'.format(os.path.basename(sys.argv[0])))
         sys.exit(1)
 
     file = args[0]
+    if not os.path.isfile(file):
+        print('Usage: {} FILE'.format(os.path.basename(sys.argv[0])))
+        sys.exit(1)
 
-# get the file lines into a list then:
     lines = []
+
     for line in open(file):
         lines.append(line.rstrip('\n'))
-
 # use enumereate on the list:
-
     for i, listelement in enumerate(lines):
-        print(i+1, listelement)
-# we increment 1 so we don't start at 0
-
-# print('Arg is "{}"'.format(arg))
-
+      #  print("{:3}".format(i), end='') 
+        print('{:3}: {}'.format((i+1),listelement))
 # --------------------------------------------------
 main()
