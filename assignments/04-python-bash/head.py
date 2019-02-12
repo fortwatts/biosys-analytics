dfecc20148e4caff360dae736a6127fb20c8cd15
gwatts <gwatts@email.arizona.edu>
2019-02-11 18:14:54 -0700

    head.py that passes test

--- assignments/04-python-bash/head.py
+++ assignments/04-python-bash/head.py
@@ -15,17 +15,21 @@ def main():
 
     if len(args) < 1:
         print('Usage: {} FILE'.format(os.path.basename(sys.argv[0])))
-        sys.exit(0)
+        sys.exit(1)
 
     if len(args) < 2:
         num_lines = 3
     else:
         num_lines = int(args[1])
 
+    if num_lines < 1:
+       print('lines ({}) must be a positive number'.format(num_lines))
+       sys.exit(1)
+
     file = args[0]
     if not os.path.isfile(file):
         print('{} is not a file'.format(file))
-        sys.exit(0)
+        sys.exit(1)
 
     lines = []
     for line in open(file):
