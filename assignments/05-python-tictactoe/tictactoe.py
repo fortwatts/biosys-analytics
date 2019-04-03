0a3c0a778482fe053e60d4889960da107309a945
fortwatts <gwatts@email.arizona.edu>
2019-04-03 10:37:56 -0700

    fixed not looking at all winning states

--- assignments/05-python-tictactoe/tictactoe.py
+++ assignments/05-python-tictactoe/tictactoe.py
@@ -104,7 +104,7 @@ def main():
             die('Cell {} already taken'.format(cell))
         state = cells
 
-    winning_states = [0,1,2]#, [4,5,6], [7,8,9], [1,4,7], [2,5,8], [3,6,9], [1,5,9], [3,5,7]]
+    winning_states = [0,1,2], [3,4,5], [6,7,8], [1,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
     print('winning_states is: {}'.format(winning_states))
     print('state is: {}'.format(state))
     print('player is: {}'.format(player))
